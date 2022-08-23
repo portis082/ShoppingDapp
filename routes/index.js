834542487beda4ab920ac5c23d01c3e94cc89c0e
portis082 <portis082@gmail.com>
2022-08-23 17:28:50 +0900

    Update routes

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@ const userController = require("../controllers/user");
 const orderController = require("../controllers/order");
 
 router.get("/product", productController.get);
-router.get("/product/{:id}", productController.getById);
+router.get("/product/:id", productController.getById);
 router.post("/product", productController.addProduct);
 
 router.get("/user", userController.get);
-router.get("/user/{:id}", userController.getById);
+router.get("/user/:id", userController.getById);
 router.post("/user", userController.createUser);
 
 router.get("/order", orderController.get);
