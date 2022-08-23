e6a8779453aee57162a00c3839d79372fb35313e
portis082 <portis082@gmail.com>
2022-08-23 17:56:20 +0900

    Add web3 connect util

---
+++ utils/connect.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const Web3 = require("web3");
+const contract = require("../build/contracts/Shopping.json");
+const web3 = new Web3(process.env.url);
+const contractAddress = contract.networks[process.env.networkid].address;
+const abi = contract.abi;
+const deployed = new web3.eth.Contract(abi, contractAddress, {
+    gas: 200000,
+    from: process.env.address
+});
+
+
+exports.deployed = deployed;
