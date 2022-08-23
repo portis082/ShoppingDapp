629c49776c4d2194975956d0429009faf7013a57
portis082 <portis082@gmail.com>
2022-08-24 05:19:52 +0900

    Update connect util

--- utils/connect.js
+++ utils/connect.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const Web3 = require("web3");
 const contract = require("../build/contracts/Shopping.json");
-const web3 = new Web3(process.env.url);
-const contractAddress = contract.networks[process.env.networkid].address;
+const web3 = new Web3(`http://${process.env.HOST}:${process.env.GANACHEPORT}`);
+const contractAddress = contract.networks[process.env.NETWORKID].address;
 const abi = contract.abi;
 const deployed = new web3.eth.Contract(abi, contractAddress, {
     gas: 1000000,
-    from: process.env.address
+    from: process.env.ADDRESS
 });
 
 
