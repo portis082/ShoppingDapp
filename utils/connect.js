4ab343a4dc41de8abd3a62abbb6b415dac190c88
portis082 <portis082@gmail.com>
2022-08-24 04:21:49 +0900

    Add utils

--- utils/connect.js
+++ utils/connect.js
@@ -5,7 +5,7 @@ const web3 = new Web3(process.env.url);
 const contractAddress = contract.networks[process.env.networkid].address;
 const abi = contract.abi;
 const deployed = new web3.eth.Contract(abi, contractAddress, {
-    gas: 200000,
+    gas: 1000000,
     from: process.env.address
 });
 
